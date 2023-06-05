3b76f0f7e7c45db7139cbdd0f9d518cf1c470e07
jackpot <s.sergeev@mediasoft.team>
2023-06-05 17:36:50 +0400

    fix templates

--- FilmRecomendationSystem/settings.py
+++ FilmRecomendationSystem/settings.py
@@ -128,11 +128,12 @@ PARLER_LANGUAGES = {
 LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale'), ]
 USE_TZ = True
 
+
 STATIC_URL = 'static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
